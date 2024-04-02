ace7fc56e72c5c88d2c95a6b64321fb0e7b176fa
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-01 22:08:05 -0300

    feat(domain): :sparkles: add new Entity struct and related functions

---
+++ domain/entity.go
@@ -0,0 +1,32 @@
+package domain
+
+type BaseEntity struct {}
+
+type Entity interface {
+	ID() interface{}
+}
+
+type entityInstance struct {
+	id interface{}
+}
+
+type EntityOption func(*entityInstance) error
+
+func NewEntity(opts ...EntityOption) Entity {
+	e := &entityInstance{}
+	for _, opt := range opts {
+		opt(e)
+	}
+	return e
+}
+
+func (e *entityInstance) ID() interface{} {
+	return e.id
+}
+
+func WithID(id interface{}) EntityOption {
+	return func(e *entityInstance) error {
+		e.id = id
+		return nil
+	}
+}
