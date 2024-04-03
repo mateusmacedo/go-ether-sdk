c1b689c72e8a978f1ccb015c9f2ae54748d624ee
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-03 09:50:48 -0300

    test(fixture): :test_tube: add test for findByNameHandler_Handle function

---
+++ test/fixture/blog/application/handler/find_by_name_test.go
@@ -0,0 +1,66 @@
+package handler_test
+
+import (
+	"testing"
+
+	apperr "github.com/mateusmacedo/go-ether-sdk/application/err"
+	appmsg "github.com/mateusmacedo/go-ether-sdk/application/message"
+
+	"github.com/mateusmacedo/go-ether-sdk/test/fixture/blog/application/handler"
+	"github.com/mateusmacedo/go-ether-sdk/test/fixture/blog/domain/repository"
+	mockRep "github.com/mateusmacedo/go-ether-sdk/test/fixture/blog/mocks/domain/repository"
+)
+
+func Test_findByNameHandler_Handle(t *testing.T) {
+	type fields struct {
+		repo repository.FindByName
+	}
+	type args struct {
+		m appmsg.Message
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    appmsg.Message
+		wantErr bool
+		errWant error
+	}{
+		{
+			name: "Test should return error when message is nil",
+			fields: fields{
+				repo: func() repository.FindByName {
+					repoImpl := mockRep.NewFindByName(t)
+					return repoImpl
+				}(),
+			},
+			args: args{
+				m: nil,
+			},
+			want:    nil,
+			wantErr: true,
+			errWant: apperr.ErrMessageContentEmpty,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(tR *testing.T) {
+			sut := handler.NewFindByNameHandler(handler.WithFindByNameRepository(tt.fields.repo))
+
+			got, err := sut.Handle(tt.args.m)
+
+			if (err != nil) != tt.wantErr {
+				tR.Errorf("findByNameHandler.Handle() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if err != nil && err != tt.errWant {
+				tR.Errorf("registerAuthorHandler.Handle() error = %v, wantErr %v", err, tt.errWant)
+				return
+			}
+
+			if got != tt.want {
+				tR.Errorf("findByNameHandler.Handle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
