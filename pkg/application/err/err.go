d9572c6615f4a43196c76dbe772c18dd31030c73
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-03 07:12:21 -0300

    fix(pkg/application/err): :bug: add error types for invalid message content and internal handler

--- pkg/application/err/err.go
+++ pkg/application/err/err.go
@@ -6,4 +6,6 @@ var (
 	ErrHandlerNotImplemented = errors.New("Handler not implemented")
 	ErrMessageNotSupported   = errors.New("message type not supported")
 	ErrMessageContentEmpty   = errors.New("message content is empty")
+	ErrInvalidMessageContent = errors.New("message content is invalid")
+	ErrInternalHandler       = errors.New("internal handler error")
 )
