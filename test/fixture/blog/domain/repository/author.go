eb2c09fdbf62753fa120445b9c5b0d2529fd28b6
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-03 05:33:40 -0300

    feat(repository): :sparkles: add author repository and author service

---
+++ test/fixture/blog/domain/repository/author.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"github.com/mateusmacedo/go-ether-sdk/test/fixture/blog/domain/model"
+)
+
+type FindByName interface {
+	FindByName(name string) (model.Author, error)
+}
+
+type AuthorRepository interface {
+	FindByName
+	Persist(author model.Author) error
+}
---
+++ test/fixture/blog/domain/service/author.go
@@ -0,0 +1,9 @@
+package service
+
+import (
+	"github.com/mateusmacedo/go-ether-sdk/test/fixture/blog/domain/model"
+)
+
+type RegisterNewAuthorService interface {
+	RegisterNewAuthor(author model.Author) error
+}
